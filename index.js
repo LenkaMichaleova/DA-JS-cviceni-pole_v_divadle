a0ca3b5f92738c9707f3d7aceeadd316b73edbd4
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-08 15:56:38 +0200

    vyřešeno

---
+++ index.js
@@ -0,0 +1,21 @@
+const pole = [30, 43, 58, 29, 4, 1]
+const zaplnenost = [3.5, 6.3, 5.7, 6.8, 2.3]
+const plays = ["Harry Potter", "Pán prstenů", "Princezna ze mlejna"]
+const reviews = [
+    {name: "casopis1", hodnoceni: 4,},
+    {name: "casopis2", hodnoceni: 10,},
+    {name: "casopis3", hodnoceni: 3,},
+    {name: "casopis4", hodnoceni: 8,},
+    {name: "casopis5", hodnoceni: 7,},
+]
+
+pole.push(23)
+console.log(pole)
+
+zaplnenost.pop()
+console.log(zaplnenost)
+
+const druhePredstaveni = plays[1]
+console.log(druhePredstaveni)
+
+console.log(reviews)
